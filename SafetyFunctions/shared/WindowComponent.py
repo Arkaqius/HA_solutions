e533609033423308fe0a7bb8a332423494e865bc
Arkaqius <kuleta.arkadiusz@gmail.com>
2024-01-03 22:41:35 +0100

    add cfg reads

--- SafetyFunctions/shared/WindowComponent.py
+++ SafetyFunctions/shared/WindowComponent.py
@@ -15,14 +15,27 @@ class WindowComponent(SafetyComponent):
         :param hass_app: The Home Assistant application instance.
         """
         super().__init__(hass_app)
-        self.safety_mechanisms = [
-            SafetyMechanism(self.hass_app,
-                            self.sm_wmc_2,
-                            'SM_WMC_2 - office',
-                            window_sensors=["binary_sensor.office_window_contact_contact"],
-                            temperature_sensor="sensor.office_temperature")
-        ]
-
+        self.safety_mechanisms = []
+        
+        # Get config
+        self.hass_app.log(self.hass_app.args)
+        
+        for sm_cfg in self.hass_app.args['safety_mechanisms']['WindowComponent']['SM_WMC_2']:
+            name = sm_cfg['name']
+            window_sensors = sm_cfg['window_sensors']
+            temperature_sensor = sm_cfg['temperature_sensor']
+            if(self.validate_entities(  {'window_sensors' : window_sensors, 'temperature_sensor': temperature_sensor},
+                                        {'window_sensors': List[str], 'temperature_sensor' : str})):
+                self.safety_mechanisms.append(
+                                SafetyMechanism(self.hass_app,
+                                self.sm_wmc_2,
+                                name,
+                                window_sensors=window_sensors,
+                                temperature_sensor=temperature_sensor)
+                )
+            else:
+                self.hass_app.log(f'SM {name} was not created due error', level='ERROR')
+        
     @safety_mechanism_decorator
     def sm_wmc_2(self, **kwargs):
         """
